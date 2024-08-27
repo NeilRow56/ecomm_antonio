78c0ac8f7eb76e9c0008314b4136520209e6cdd6
NeilRow56 <neilrowland56@gmail.com>
2024-08-27 04:07:42 +0100

    StoreSwitcher added to navabar

--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -1,11 +1,26 @@
 import { UserButton } from "@clerk/nextjs";
 import { MainNav } from "./MainNav";
+import StoreSwitcher from "./StoreSwitcher";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import db from "@/lib/db";
 
-export const Navbar = () => {
+export const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const stores = await db.store.findMany({
+    where: {
+      userId,
+    },
+  });
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>Store switcher</div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton />
